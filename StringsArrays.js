335463b883834b785e484e272e2610d0978fad78
Douglas Oliveira <109635136+Djgoliveira@users.noreply.github.com>
2022-11-09 16:16:23 -0300

    Aula 10
    String e Arrays: Trabalhando com Coleções

---
+++ StringsArrays.js
@@ -0,0 +1,41 @@
+/*
+Extrair elementos de um array.
+Comparar elementos de um array com os elementos de outro. 
+O que esses códigos retornam?
+Neste exercício, você terá que pensar sobre o que esses códigos retornam sem testá-lo no console.
+let numbers =[22, 33, 54, 66, 72]
+console.log(numbers[numbers.length])
+
+
+let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ]
+console.log(grupoDeAmigos[1][0])
+
+
+let str = "uma string qualquer”
+let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ]
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1])
+Array Invertido
+Neste exercício, você terá que criar uma função que retorne um array com seus elementos invertidos, sem modificá-lo. Então, você deve fazer uma função que o modifique e inverta a ordem de seus elementos.
+Crie a função imprimirInverso que pega um Array como um argumento e imprime cada elemento em ordem reversa no console (você não precisa inverter o Array).
+Crie a função inverter que recebe um Array como argumento e retorna um novo invertido.
+somarArray()
+Neste exercício, você criará uma função somarArray() que aceita um array de números e retorna a soma de todos eles.
+Exemplo:
+somarArray([1,2,3])                // 6
+somarArray([10, 3, 10, 4])     // 27
+somarArray([-5,100])             // 95
+Simulação Array.join()
+Neste exercício, você criará uma função chamada join que recebe um array e simula o comportamento do método Array.join().
+Importante: Não poderá utilizar o método Array.join() original.
+Por exemplo:
+join(["o","l","á"]) deve retornar a string "olá".
+join([“t”,"c","h","a,"u"]) deve retornar a string "tchau".
+
+Coleções de Filmes (e mais…)
+O tech leader da equipe precisa conhecer a estrutura de dados do novo projeto. Para isso, devemos:
+Criar a estrutura apropriada para armazenar os seguintes filmes e séries:
+	"star wars", "matrix",  "mr. robot", "o preço do amanhã",  "a vida é bela". 
+Importante: verifique se tudo funciona corretamente acessando qualquer um dos filmes uma vez que a estrutura correspondente tenha sido criada.
+
+Os filmes devem estar todos em letras maiúsculas. Para isso, crie uma função que recebe um array por parâmetro e converta o conteúdo de cada elemento 
+/*
