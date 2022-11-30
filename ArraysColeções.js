2405b0407f836f6eb49796e1c7bda453f62b5c79
Douglas Oliveira <109635136+Djgoliveira@users.noreply.github.com>
2022-11-30 14:52:55 -0300

    Arrays Coleções

---
+++ ArraysColeções.js
@@ -0,0 +1,15 @@
+let numbers =[22, 33, 54, 66, 72];
+console.log(numbers[numbers.length]);
+//Mostra a quantidade de Dados dentro do Array Number 05
+
+let grupoDeAmigo = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk",
+"Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ];
+console.log(grupoDeAmigo[1][0]);
+//Mostra a quantidade de Dados dentro do conjunto do Array ele vai entrar no conjunto 1 no primeiro item desse conjunto que é o Spiderman
+
+let str = "una string qualquer"
+let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string",
+"123","false", 54, true, str] ];
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]);
+//Mostra a quantidade de Dados dentro do conjunto do Array ele vai entrar no conjunto 2 , depois ir até o dado chamado false e contar até o final do array e voltar 1 casa e parar no true
+//e executar a leitura a partir dali no caso o proximo dado str.
